c66d32fd478a0b4656ba9b599a7b9ed7791c03f0
Yusuf YILDIRIM <lostofis@gmail.com>
2017-04-13 15:52:30 +0300

    ChatScreen added to index.

--- app/screens/index.js
+++ app/screens/index.js
@@ -1,8 +1,10 @@
 import { Navigation } from 'react-native-navigation';
 
 import HomeScreen from './Home';
+import ChatScreen from './Chat';
 
 // register all screens of the app (including internal ones)
 export default function registerScreens() {
   Navigation.registerComponent('HomeScreen', () => HomeScreen);
+  Navigation.registerComponent('ChatScreen', () => ChatScreen);
 }
