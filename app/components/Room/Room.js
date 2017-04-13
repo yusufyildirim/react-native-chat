c130ce284b1c3e2f7519d11307800f644f6b8970
Yusuf YILDIRIM <lostofis@gmail.com>
2017-04-13 18:28:48 +0300

    Room Component became Touchable and onPress Prop added.

--- app/components/Room/Room.js
+++ app/components/Room/Room.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Row, Subtitle, Caption, Image, Text, Divider } from '@shoutem/ui';
+import { View, Row, Subtitle, Caption, Image, Text, Divider, TouchableOpacity } from '@shoutem/ui';
 import styles from './styles';
 
 class Room extends Component {
@@ -9,31 +9,34 @@ class Room extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Row>
-          {this.props.unreadMessages ? <View styleName="notification-dot" /> : null}
-          <Image
-            style={styles.avatar}
-            source={{ uri: this.props.avatar }}
-          />
-          <View styleName="horizontal space-between">
-            <View styleName="flexible vertical space-between">
-              <Subtitle numberOfLines={1}>{this.props.name}</Subtitle>
-              <Text numberOfLines={1}>{this.props.lastMessage}</Text>
-            </View>
+      <TouchableOpacity onPress={this.props.onPress}>
+        <View style={styles.container}>
+          <Row>
+            {this.props.unreadMessages ? <View styleName="notification-dot" /> : null}
+            <Image
+              style={styles.avatar}
+              source={{ uri: this.props.avatar }}
+            />
+            <View styleName="horizontal space-between">
+              <View styleName="flexible vertical space-between">
+                <Subtitle numberOfLines={1}>{this.props.name}</Subtitle>
+                <Text numberOfLines={1}>{this.props.lastMessage}</Text>
+              </View>
 
-            <View>
-              <Caption>{this.props.lastMessageTime}</Caption>
+              <View>
+                <Caption>{this.props.lastMessageTime}</Caption>
+              </View>
             </View>
-          </View>
-        </Row>
-        <Divider styleName="line" />
-      </View>
+          </Row>
+          <Divider styleName="line" />
+        </View>
+      </TouchableOpacity>
     );
   }
 }
 
 Room.propTypes = {
+  onPress: PropTypes.func,
   unreadMessages: PropTypes.bool,
   avatar: PropTypes.string,
   name: PropTypes.string.isRequired,
@@ -42,6 +45,7 @@ Room.propTypes = {
 };
 
 Room.defaultProps = {
+  onPress: () => {},
   unreadMessages: false,
   avatar: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png',
   // name: '-',
