9de009955cd2c24f7832a9b113f1ca631d2a0d65
Yusuf YILDIRIM <lostofis@gmail.com>
2017-04-13 12:24:44 +0300

    Room component's layout done.

--- app/components/Room/Room.js
+++ app/components/Room/Room.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import { Row } from '@shoutem/ui';
+import { View, Row, Subtitle, Caption, Image, Text, Divider } from '@shoutem/ui';
+import styles from './styles';
 
 class Room extends Component {
   static navigatorStyle = {
@@ -8,7 +9,26 @@ class Room extends Component {
 
   render() {
     return (
-      <Row />
+      <View style={styles.container}>
+        <Row>
+          <View styleName="notification-dot" />
+          <Image
+            style={styles.avatar}
+            source={{ uri: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png' }}
+          />
+          <View styleName="horizontal space-between">
+            <View styleName="flexible vertical space-between">
+              <Subtitle numberOfLines={1}>Foo Room</Subtitle>
+              <Text numberOfLines={1}>Test Message bla bla bla aıoshdıo ashdoı asdoı</Text>
+            </View>
+
+            <View>
+              <Caption>20:21</Caption>
+            </View>
+          </View>
+        </Row>
+        <Divider styleName="line" />
+      </View>
     );
   }
 }
