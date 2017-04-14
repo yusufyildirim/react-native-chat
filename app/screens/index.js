c38d4e74b79af9c5312086bdf43f78388efd6a9f
Yusuf YILDIRIM <lostofis@gmail.com>
2017-04-14 23:32:44 +0300

    NicknameScreen registered.

--- app/screens/index.js
+++ app/screens/index.js
@@ -1,10 +1,12 @@
 import { Navigation } from 'react-native-navigation';
 
 import HomeScreen from './Home';
+import NicknameScreen from './Nickname';
 import ChatScreen from './Chat';
 
 // register all screens of the app (including internal ones)
 export default function registerScreens() {
+  Navigation.registerComponent('NicknameScreen', () => NicknameScreen);
   Navigation.registerComponent('HomeScreen', () => HomeScreen);
   Navigation.registerComponent('ChatScreen', () => ChatScreen);
 }
